5f36025f95666a86bb6a1b841f16c8834afb7f86
AndersonVanzo <anderson.vanzo01@gmail.com>
2023-08-27 10:17:18 -0300

    feat(GuestFormActivity): layout implemented

--- app/src/main/java/com/knownt/guests/GuestFormActivity.kt
+++ app/src/main/java/com/knownt/guests/GuestFormActivity.kt
@@ -1,11 +1,24 @@
 package com.knownt.guests
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.knownt.guests.databinding.ActivityGuestFormBinding
 
 class GuestFormActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[GuestFormViewModel::class.java]
+
+        binding.buttonSave.setOnClickListener { onSaveButtonClick() }
+        binding.radioPresent.isChecked = true
     }
+
+    private fun onSaveButtonClick() {}
 }
---
+++ app/src/main/java/com/knownt/guests/GuestFormViewModel.kt
@@ -0,0 +1,5 @@
+package com.knownt.guests
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel()
